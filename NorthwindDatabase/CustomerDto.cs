345586a4912137d3b26ce1590abfc395aeb81b0d
Terrence C <mc.terrence.castrence@gmail.com>
2021-08-08 17:04:28 -0500

    Add project files.

---
+++ NorthwindDatabase/CustomerDto.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindDatabase
+{
+    [Serializable()]
+    public class CustomerDto : ISerializable
+    {
+        public int Id { get; set; }
+        public string Company { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string EmailAddress { get; set; }
+        public string JobTitle { get; set; }
+        public string BusinessPhone { get; set; }
+        public string HomePhone { get; set; }
+        public string MobilePhone { get; set; }
+        public string FaxNumber { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string StateProvince { get; set; }
+        public string ZipPostalCode { get; set; }
+        public string CountryRegion { get; set; }
+        public string WebPage { get; set; }
+        public string Notes { get; set; }
+        public byte[] Attachments { get; set; }
+
+        public CustomerDto() { }
+
+        public CustomerDto(SerializationInfo info, StreamingContext context)
+        {
+            Id = (int)info.GetValue("Id", typeof(int));
+            Company = (string)info.GetValue("CusLName", typeof(string));
+            LastName = (string)info.GetValue("LastName", typeof(string));
+            FirstName = (string)info.GetValue("FirstName", typeof(string));
+            EmailAddress = (string)info.GetValue("EmailAddress", typeof(string));
+            JobTitle = (string)info.GetValue("JobTitle", typeof(string));
+            BusinessPhone = (string)info.GetValue("BusinessPhone", typeof(string));
+            HomePhone = (string)info.GetValue("HomePhone", typeof(string));
+            MobilePhone = (string)info.GetValue("MobilePhone", typeof(string));
+            FaxNumber = (string)info.GetValue("FaxNumber", typeof(string));
+            Address = (string)info.GetValue("Address", typeof(string));
+            City = (string)info.GetValue("City", typeof(string));
+            StateProvince = (string)info.GetValue("StateProvince", typeof(string));
+            ZipPostalCode = (string)info.GetValue("ZipPostalCode", typeof(string));
+            CountryRegion = (string)info.GetValue("CountryRegion", typeof(string));
+            WebPage = (string)info.GetValue("WebPage", typeof(string));
+            Notes = (string)info.GetValue("Notes", typeof(string));
+            Attachments = (byte[])info.GetValue("Attachments", typeof(byte[]));
+        }
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Id", Id);
+            info.AddValue("Company", Company);
+            info.AddValue("LastName", LastName);
+            info.AddValue("FirstName", FirstName);
+            info.AddValue("EmailAddress", EmailAddress);
+            info.AddValue("JobTitle", JobTitle);
+            info.AddValue("BusinessPhone", BusinessPhone);
+            info.AddValue("HomePhone", HomePhone);
+            info.AddValue("MobilePhone", MobilePhone);
+            info.AddValue("FaxNumber", FaxNumber);
+            info.AddValue("Address", Address);
+            info.AddValue("City", City);
+            info.AddValue("StateProvince", StateProvince);
+            info.AddValue("ZipPostalCode", ZipPostalCode);
+            info.AddValue("CountryRegion", CountryRegion);
+            info.AddValue("WebPage", WebPage);
+            info.AddValue("Notes", Notes);
+            info.AddValue("Attachments", Attachments);
+        }
+    }
+}
---
+++ NorthwindDatabase/Program.cs
@@ -0,0 +1,151 @@
+﻿using NorthwindDatabase.DbModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Text.Json;
+using System.Xml.Serialization;
+using static System.Console;
+
+namespace NorthwindDatabase
+{
+    class Program
+    {
+        private static readonly northwindContext _context = new northwindContext();
+
+        static void Main(string[] args)
+        {
+            var customer = _context.Customers.ToList();
+            var customerDto = customer.Select(c => new CustomerDto
+            {
+                Id = c.Id,
+                Company = c.Company,
+                LastName = c.LastName,
+                FirstName = c.FirstName,
+                EmailAddress = c.EmailAddress,
+                JobTitle = c.JobTitle,
+                BusinessPhone = c.BusinessPhone,
+                HomePhone = c.HomePhone,
+                MobilePhone = c.MobilePhone,
+                FaxNumber = c.FaxNumber,
+                Address = c.Address,
+                City = c.City,
+                StateProvince = c.StateProvince,
+                ZipPostalCode = c.ZipPostalCode,
+                CountryRegion = c.CountryRegion,
+                WebPage = c.WebPage,
+                Notes = c.Notes,
+                Attachments = c.Attachments,
+            });
+
+            var products = _context.Products.ToList();
+            var productsDto = new List<ProductDto>();
+
+            foreach (var p in products)
+            {
+                ProductDto prod = new ProductDto
+                {
+                    SupplierIds = p.SupplierIds,
+                    Id = p.Id,
+                    ProductCode = p.ProductCode,
+                    ProductName = p.ProductName,
+                    Description = p.Description,
+                    StandardCost = p.StandardCost,
+                    ListPrice = p.ListPrice,
+                    ReorderLevel = p.ReorderLevel,
+                    TargetLevel = p.TargetLevel,
+                    QuantityPerUnit = p.QuantityPerUnit,
+                    Discontinued = p.Discontinued,
+                    MinimumReorderQuantity = p.MinimumReorderQuantity,
+                    Category = p.Category,
+                    Attachments = p.Attachments,
+                };
+                productsDto.Add(prod);
+            }
+
+            // CustomerDto
+            string xmlCustomersDto = "customersDto.xml";
+            ToXmlFile(xmlCustomersDto, productsDto);
+
+            string jsonCustomersDto = "customersDto.json";
+            ToJsonFile(jsonCustomersDto, productsDto);
+
+            string binaryCustomersDto = "customersDto.dat";
+            ToBinaryFile(binaryCustomersDto, productsDto);
+
+            // ProductDto
+            string xmlProductsDto = "productsDto.xml";
+            ToXmlFile(xmlProductsDto, productsDto);
+
+            string jsonProductsDto = "productsDto.json";
+            ToJsonFile(jsonProductsDto, productsDto);
+
+            string binaryProductsDto = "productsDto.dat";
+            ToBinaryFile(binaryProductsDto, productsDto);
+
+            List<SerializedFile> fileList = new List<SerializedFile>
+            {
+                new SerializedFile{
+                    Name = xmlCustomersDto,
+                    Size = new FileInfo(xmlCustomersDto).Length },
+                new SerializedFile{
+                    Name = jsonCustomersDto,
+                    Size = new FileInfo(jsonCustomersDto).Length },
+                new SerializedFile{
+                    Name = binaryCustomersDto,
+                    Size = new FileInfo(binaryCustomersDto).Length },
+                new SerializedFile{
+                    Name = xmlProductsDto,
+                    Size = new FileInfo(xmlProductsDto).Length },
+                new SerializedFile{
+                    Name = jsonProductsDto,
+                    Size = new FileInfo(jsonProductsDto).Length },
+                new SerializedFile{
+                    Name = binaryProductsDto,
+                    Size = new FileInfo(binaryProductsDto).Length },
+            };
+
+            fileList.Sort();
+            int place = 1;
+            foreach (var file in fileList)
+            {
+                WriteLine($"{place++}. {file.Name} : {file.Size} bytes");
+            }
+        }
+        public static void ToBinaryFile<T>(string file, T obj)
+        {
+            using (Stream st = File.Open(file, FileMode.Create))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(st, obj);
+            }
+        }
+        public static void ToJsonFile<T>(string file, T obj)
+        {
+            string json = JsonSerializer.Serialize(obj);
+            File.WriteAllText(file, json);
+        }
+        public static T FromXmlFile<T>(string file)
+        {
+            XmlSerializer xmls = new XmlSerializer(typeof(T));
+            var xmlContent = File.ReadAllText(file);
+
+            using (StringReader sr = new StringReader(xmlContent))
+            {
+                return (T)xmls.Deserialize(sr);
+            }
+        }
+        public static void ToXmlFile<T>(string file, T obj)
+        {
+            using (StringWriter sw =
+                new StringWriter(new StringBuilder()))
+            {
+                XmlSerializer xmls = new XmlSerializer(typeof(T));
+                xmls.Serialize(sw, obj);
+                File.WriteAllText(file, sw.ToString());
+            }
+        }
+    }
+}
